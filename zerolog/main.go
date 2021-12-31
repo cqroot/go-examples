081c5f3f16e6216ba205064e1461db1b2a0a3202
cqroot <cqroot@outlook.com>
2021-12-31 21:53:42 +0800

    feat: rich zerolog examples

--- zerolog/main.go
+++ zerolog/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -8,20 +9,28 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func init() {
-	zerolog.TimestampFunc = func() time.Time {
-		return time.Now().In(time.Local)
-	}
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC822}).With().Caller().Logger()
-}
-
-func main() {
+func logSomething() {
 	log.Debug().
 		Str("Scale", "833 cents").
 		Float64("Interval", 833.09).
 		Msg("Fibonacci is everywhere")
-
-	log.Debug().
+	log.Warn().
 		Str("Name", "Tom").
 		Send()
+	fmt.Println("")
+}
+
+func main() {
+	zerolog.TimestampFunc = func() time.Time {
+		return time.Now().In(time.Local)
+	}
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "2006-01-02 15:04:05 Mon"})
+	logSomething()
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "2006-01-02 15:04:05 Mon"}).With().Caller().Logger()
+	logSomething()
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	logSomething()
 }
